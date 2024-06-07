5dffeef39463ae6a3418c208fa6b1a7103a74dca
Tay <tt262767@gmail.com>
2024-06-07 16:01:14 -0400

    final set

--- src/Maps.java
+++ src/Maps.java
@@ -2,5 +2,7 @@ public class Maps {
     public static void main(String[] args) {
         System.out.printf("This is a map");
         System.out.printf("Me gustas mapa");
+        System.out.printf("Me gustas mapa");
+
     }
 }
